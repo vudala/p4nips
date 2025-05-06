6f3a37b7d5bb5df342407e33d7c3c19648a2c055
vudala <vudalae@gmail.com>
2025-05-06 20:26:04 +0200

    fix

--- start_switch_test.sh
+++ start_switch_test.sh
@@ -19,9 +19,9 @@ tmux send-keys -t 0 "./run_tofino_model.sh -p $P4TEST --arch tofino2" C-m
 mkdir -p $SDE/logs
 
 tmux send-keys -t 1 "cd $SDE" C-m
-tmux send-keys -t 1 "./run_switchd.sh -p $P4TEST --arch tofino2 --log-dir $SDE/logs" C-m
+tmux send-keys -t 1 "./run_switchd.sh -p $P4TEST --arch tofino2" C-m
 
 tmux send-keys -t 2 "cd $SDE" C-m
-tmux send-keys -t 2 "./run_p4_tests.sh -p $P4TEST --arch ofino2" C-m
+tmux send-keys -t 2 "./run_p4_tests.sh -p $P4TEST --arch tofino2" C-m
 
 tmux attach-session -t switch 
